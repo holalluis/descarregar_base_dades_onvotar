bc7c36f72a4d64ec7520af433760a27fa808a1fb
lbosch <lbosch@icra.cat>
2017-09-24 20:18:32 +0200

    afegits scripts per descarregar la web i sincronitzar amb el servidor

---
+++ scripts/dump.sh
@@ -0,0 +1,3 @@
+
+url="https://onvotar.garantiespelreferendum.com/db/00/01.db"
+wget --no-check-certificate --mirror --convert-links --adjust-extension --page-requisites --no-parent "$url"
---
+++ scripts/rsync.sh
@@ -0,0 +1,3 @@
+
+#desplega la web al servidor
+rsync -avzh -e ssh onvotar.garantiespelreferendum.com/ pi@192.168.1.132:~/onvotar
